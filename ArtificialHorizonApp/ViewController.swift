f2f7fb04cfcf3cdda192e68b63ccb21469d594fa
mrgone12 <piotrek18334@gmail.com>
2020-03-27 16:59:17 +0100

    Initial Commit

---
+++ ArtificialHorizonApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ArtificialHorizonApp
+//
+//  Created by Piotr on 27/03/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
