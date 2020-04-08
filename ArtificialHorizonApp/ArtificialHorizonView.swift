5a9251092c19cc14f888cd21132787af00d242c5
mrgone12 <piotrek18334@gmail.com>
2020-04-08 14:43:22 +0200

    first commit

---
+++ ArtificialHorizonApp/ArtificialHorizonView.swift
@@ -0,0 +1,21 @@
+//
+//  ArtificialHorizonView.swift
+//  ArtificialHorizonApp
+//
+//  Created by Piotr on 27/03/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import UIKit
+
+class ArtificialHorizonView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ ArtificialHorizonApp/Constants.swift
@@ -0,0 +1,9 @@
+//
+//  Constants.swift
+//  ArtificialHorizonApp
+//
+//  Created by Piotr on 27/03/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import Foundation
